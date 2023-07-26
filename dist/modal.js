8afc2d5aa7702dea1a1f7d111fc0875c70ee40ee
Dmitry R <dimsog@gmail.com>
2023-07-27 00:31:01 +0300

    feat: header improvements

--- dist/modal.js
+++ dist/modal.js
@@ -1,12 +1,33 @@
-import { defineComponent as _, ref as a, onMounted as m, onUnmounted as v, openBlock as c, createElementBlock as i, createElementVNode as n, toDisplayString as h, renderSlot as f, createCommentVNode as y, pushScopeId as g, popScopeId as S, nextTick as M } from "vue";
-const s = /* @__PURE__ */ new Map(), k = (e, t) => {
-  s.set(e, t);
-}, r = (e) => {
-  const t = s.get(e);
+import { defineComponent as g, ref as a, onMounted as y, onUnmounted as k, openBlock as v, createElementBlock as m, createElementVNode as n, toDisplayString as w, withModifiers as x, renderSlot as M, createCommentVNode as b, nextTick as S, pushScopeId as E, popScopeId as C } from "vue";
+const i = /* @__PURE__ */ new Map(), B = (e, t) => {
+  i.set(e, t);
+}, h = (e) => {
+  const t = i.get(e);
   return t === void 0 ? null : t;
-}, x = (e) => {
-  s.delete(e);
-}, w = (e) => (g("data-v-c22269f4"), e = e(), S(), e), I = { class: "modal-header" }, E = { class: "modal-header__title" }, $ = /* @__PURE__ */ w(() => /* @__PURE__ */ n("div", { class: "modal-header__buttons" }, null, -1)), b = { class: "modal-body" }, q = /* @__PURE__ */ _({
+}, I = (e) => {
+  i.delete(e);
+}, $ = (e) => (E("data-v-d29e728b"), e = e(), C(), e), L = { class: "modal-header__title" }, H = { class: "modal-header__buttons" }, q = ["onClick"], N = /* @__PURE__ */ $(() => /* @__PURE__ */ n("svg", {
+  xmlns: "http://www.w3.org/2000/svg",
+  class: "icon icon-tabler icon-tabler-x",
+  width: "24",
+  height: "24",
+  viewBox: "0 0 24 24",
+  "stroke-width": "2",
+  stroke: "currentColor",
+  fill: "none",
+  "stroke-linecap": "round",
+  "stroke-linejoin": "round"
+}, [
+  /* @__PURE__ */ n("path", {
+    stroke: "none",
+    d: "M0 0h24v24H0z",
+    fill: "none"
+  }),
+  /* @__PURE__ */ n("path", { d: "M18 6l-12 12" }),
+  /* @__PURE__ */ n("path", { d: "M6 6l12 12" })
+], -1)), O = [
+  N
+], R = { class: "modal-body" }, V = /* @__PURE__ */ g({
   __name: "Modal",
   props: {
     name: {
@@ -20,60 +41,77 @@ const s = /* @__PURE__ */ new Map(), k = (e, t) => {
     },
     width: {
       type: String,
-      default: "800px"
+      default: "600px"
     },
     height: {
       type: String,
-      default: "600px"
+      default: "400px"
     }
   },
   setup(e) {
-    const t = e, l = a(!1), o = a(null), d = () => {
-      l.value = !0, M(() => {
-        o.value !== null && (o.value.style.width = t.width, o.value.style.height = t.height, o.value.style.top = document.documentElement.clientHeight / 2 - o.value.clientHeight / 2, o.value.style.left = document.documentElement.clientWidth / 2 - o.value.clientWidth / 2);
+    const t = e, l = a(!1), o = a(null), d = a(null), _ = () => {
+      l.value = !0, S(() => {
+        o.value !== null && (o.value.style.width = t.width, o.value.style.height = t.height, o.value.style.top = document.documentElement.clientHeight / 2 - o.value.clientHeight / 2, o.value.style.left = document.documentElement.clientWidth / 2 - o.value.clientWidth / 2, d.value !== null && d.value.addEventListener("mousedown", (s) => {
+          let r = s.clientX - o.value.getBoundingClientRect().left, f = s.clientY - o.value.getBoundingClientRect().top;
+          const u = function(p) {
+            o.value.style.top = p.pageY - f + "px", o.value.style.left = p.pageX - r + "px";
+          };
+          document.addEventListener("mousemove", u), d.value.addEventListener("mouseup", () => {
+            document.removeEventListener("mousemove", u);
+          });
+        }));
       });
-    }, u = () => {
+    }, c = () => {
       l.value = !1;
     };
-    return m(() => {
-      k(t.name, {
-        open: d,
-        close: u
+    return y(() => {
+      B(t.name, {
+        open: _,
+        close: c
       });
-    }), v(() => {
-      x(t.name);
-    }), (p, C) => (c(), i("div", null, [
-      l.value ? (c(), i("div", {
+    }), k(() => {
+      I(t.name);
+    }), (s, r) => (v(), m("div", null, [
+      l.value ? (v(), m("div", {
         key: 0,
         ref_key: "$modal",
         ref: o,
         class: "modal"
       }, [
-        n("div", I, [
-          n("div", E, h(e.title), 1),
-          $
-        ]),
-        n("div", b, [
-          f(p.$slots, "default", {}, void 0, !0)
+        n("div", {
+          ref_key: "$header",
+          ref: d,
+          class: "modal-header"
+        }, [
+          n("div", L, w(e.title), 1),
+          n("div", H, [
+            n("button", {
+              type: "button",
+              onClick: x(c, ["prevent"])
+            }, O, 8, q)
+          ])
+        ], 512),
+        n("div", R, [
+          M(s.$slots, "default", {}, void 0, !0)
         ])
-      ], 512)) : y("", !0)
+      ], 512)) : b("", !0)
     ]));
   }
 });
-const B = (e, t) => {
+const W = (e, t) => {
   const l = e.__vccOpts || e;
   for (const [o, d] of t)
     l[o] = d;
   return l;
-}, N = /* @__PURE__ */ B(q, [["__scopeId", "data-v-c22269f4"]]), O = (e) => {
+}, Y = /* @__PURE__ */ W(V, [["__scopeId", "data-v-d29e728b"]]), j = (e) => {
   var t;
-  (t = r(e)) == null || t.open();
-}, V = (e) => {
+  (t = h(e)) == null || t.open();
+}, z = (e) => {
   var t;
-  (t = r(e)) == null || t.close();
+  (t = h(e)) == null || t.close();
 };
 export {
-  N as Modal,
-  V as close,
-  O as open
+  Y as Modal,
+  z as close,
+  j as open
 };
--- src/Components/Modal.vue
+++ src/Components/Modal.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <div ref="$modal" class="modal" v-if="modalIsOpened">
-      <div class="modal-header">
+      <div ref="$header" class="modal-header">
         <div class="modal-header__title">
           {{ title }}
         </div>
         <div class="modal-header__buttons">
-
+          <button type="button" @click.prevent="close">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M18 6l-12 12"></path>
+              <path d="M6 6l12 12"></path>
+            </svg>
+          </button>
         </div>
       </div>
       <div class="modal-body">
@@ -42,6 +48,7 @@ const props = defineProps({
 });
 
 const $modal = ref(null);
+const $header = ref(null);
 
 const open = (): void => {
   modalIsOpened.value = true;
@@ -54,6 +61,26 @@ const open = (): void => {
     $modal.value.style.height = props.height;
     $modal.value.style.top = document.documentElement.clientHeight / 2 - $modal.value.clientHeight / 2;
     $modal.value.style.left = document.documentElement.clientWidth / 2 - $modal.value.clientWidth / 2;
+
+    if ($header.value === null) {
+      return;
+    }
+
+    $header.value.addEventListener('mousedown', (e) => {
+      let x = e.clientX - $modal.value.getBoundingClientRect().left;
+      let y = e.clientY - $modal.value.getBoundingClientRect().top;
+
+      const move = function (e) {
+        $modal.value.style.top = (e.pageY - y) + 'px';
+        $modal.value.style.left = (e.pageX - x) + 'px';
+      }
+
+      document.addEventListener('mousemove', move);
+
+      $header.value.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', move);
+      })
+    });
   })
 };
 
@@ -75,18 +102,32 @@ onUnmounted((): void => {
 
 <style lang="scss" scoped>
 .modal {
+  font-family: sans-serif;
   position: absolute;
   z-index: 1000;
   background: #fff;
   border-radius: 8px;
   border: 1px solid rgba(30, 29, 29, 0.16);
   box-shadow: 0 0 10px 0 rgba(0,0,0,0.10);
+  overflow: hidden;
 
   .modal-header {
     display: flex;
+    background: #f3f4f6;
+    justify-content: space-between;
+    align-items: center;
+    user-select: none;
 
     .modal-header__title {
-      padding: 1rem;
+      padding: .7rem;
+    }
+
+    .modal-header__buttons {
+      padding-right: .3rem;
+      button {
+        background: transparent;
+        border: none;
+      }
     }
   }
 
