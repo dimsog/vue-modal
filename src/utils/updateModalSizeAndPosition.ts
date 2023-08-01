4b331e7356a412e626d914b9889e0b7c72957f89
Dmitry R <dimsog@gmail.com>
2023-08-02 00:11:40 +0300

    refactor: remove space

--- src/utils/updateModalSizeAndPosition.ts
+++ src/utils/updateModalSizeAndPosition.ts
@@ -5,5 +5,4 @@ export default ($modal: HTMLElement, modalPosition: ModalPosition) => {
     $modal.style.height = modalPosition.height + 'px';
     $modal.style.top = modalPosition.y !== null ? modalPosition.y + 'px' : (document.documentElement.clientHeight / 2 - $modal.clientHeight / 2) + 'px';
     $modal.style.left = modalPosition.x !== null ? modalPosition.x + 'px' : (document.documentElement.clientWidth / 2 - $modal.clientWidth / 2) + 'px';
-
 }
