5ad1aca8e951a1523b1fa2a4166d861c13f409d5
Dmitry R <dimsog@gmail.com>
2024-05-16 23:12:35 +0300

    fix: resize modal

--- src/Types/ResizeModalOptions.ts
+++ src/Types/ResizeModalOptions.ts
@@ -1,5 +1,7 @@
 import { ModalPosition } from "./ModalPosition";
 
 export type ResizeModalOptions = {
+    minWidth: number,
+    minHeight: number,
     resize: (position: ModalPosition) => void,
 }
